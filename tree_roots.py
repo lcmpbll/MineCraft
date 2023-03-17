f98d12a6ee1ce5f67c4812ad0f9b58060d1f63cd
Liam Cammpbell <liam22campbell@gmail.com>
2023-03-17 13:46:37 -0700

    Reduce amount of trees

--- tree_roots.py
+++ tree_roots.py
@@ -12,11 +12,10 @@ class Trees:
     freq = 1.5
     amp = 100
     treeChance = ((this.noise([_x/freq, _z/freq]))*amp)
-    if treeChance > 25:
+    if treeChance > 40:
       this.plantTree(_x, _y, _z)
   def plantTree(this, _x, _y, _z):
     from random import randint
-    print("planted")
     tree = Entity(model = None, position = Vec3(_x,_y,_z))
     crown = Entity(model ='cube', scale=6, y=9, color=color.green, collider = 'none')
     trunk = Entity(model='cube', scale_y=12, scale_x = 0.6, scale_z = 0.6,  color=color.brown, collider = 'box')
