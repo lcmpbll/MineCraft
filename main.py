2ee952ec282eec23243e55815f9c970bedd58947
Liam Cammpbell <liam22campbell@gmail.com>
2023-04-02 17:09:59 -0700

    Make simple terrain

---
+++ main.py
@@ -0,0 +1,17 @@
+from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController 
+from terrain_system import MeshTerrain
+
+# this is for updating and moving character
+app = Ursina()
+#Initial Variables / imports, creations
+window.color=color.rgb(200,0,255)
+subject = FirstPersonController()
+subject.gravity = 0.0
+terrain = MeshTerrain()
+def update():
+    pass
+terrain.genTerrain()
+  
+app.run()
+
---
+++ terrain_system.py
@@ -0,0 +1,33 @@
+# from ursina import Entity, load_model, Mesh, model
+from ursina import *
+from random import randrange
+
+class MeshTerrain:
+    def __init__(this):
+        this.subsets = []
+        this.numSubsets = 1
+        this.subWidth = 128
+        this.block = load_model('block.obj')
+        this.textureAtlas = 'block_texture.png'
+        for i in range(0, this.numSubsets):
+            e = Entity(model = Mesh(), texture = this.textureAtlas)
+            e.texture_scale*=64/e.texture.width
+            this.subsets.append(e)
+    def genTerrain(this):
+        x = 0
+        z = 0
+        d = int(this.subWidth * 0.5)
+        for k in range(-d, d):
+            for j in range(-d, d):
+                y = randrange(-1, 1)
+                this.genBlock(x+k, y, z+j)
+        this.subsets[0].model.generate()      
+    def genBlock(this, _x, _y, _z):
+        # Extend to the vertices of our model, or first subset
+        model = this.subsets[0].model
+        model.vertices.extend([Vec3(_x,_y,_z) + v for v in this.block.vertices])
+        # texture atlas at coord for grass
+        uu = 8
+        uv = 7
+        model.uvs.extend([Vec2(uu, uv) + u for u in this.block.uvs])
+        
