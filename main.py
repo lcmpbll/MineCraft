ebc95a2ad77d03947dc5c0ddfc479ea046f09886
Liam Cammpbell <liam22campbell@gmail.com>
2023-04-05 18:54:17 -0700

    Add sky color, create jump, fall for subject

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from terrain_system import MeshTerrain
 # this is for updating and moving character
 app = Ursina()
 #Initial Variables / imports, creations
-window.color=color.rgb(200,0,255)
+window.color=color.rgb(0,200,255)
+indra = Sky()
+indra.color = window.color
 subject = FirstPersonController()
 subject.gravity = 0.0
 terrain = MeshTerrain()
@@ -15,6 +17,24 @@ def input(key):
         app.userExit()
 
 def update():
+    blockFound = False
+    step = 2
+    height = 1.86
+    # Technically flooring twice, but prevents shaking
+    x = floor(subject.x + 0.5)
+    y = floor(subject.y + 0.5)
+    z = floor(subject.z + 0.5)
+    for i in range(-step, step):
+        if terrain.getTerrainDic(x, y + i, z) == 't': 
+            target = y + i + height
+            blockFound = True
+            break
+    if blockFound == True: 
+        # step up or down : >
+        subject.y = lerp(subject.y, target, 6 * time.dt)
+    else: 
+        #gravity fall : <
+        subject.y -= 9.8 * time.dt 
     pass
 terrain.genTerrain()
   
