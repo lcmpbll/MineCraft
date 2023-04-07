62564b97f76b9580767eb763da1c1c96c64437f0
Liam Cammpbell <liam22campbell@gmail.com>
2023-04-07 15:24:35 -0700

    Import slim, add Water!

--- terrain_system.py
+++ terrain_system.py
@@ -1,12 +1,12 @@
-# from ursina import Entity, load_model, Mesh, model
-from ursina import *
+#from ursina import *
+from ursina import Entity, floor, Mesh, Vec3, Vec2, Vec4, load_model
 from random import randrange, random
 from perlin import Perlin
 from swirl_engine import SwirlEngine
-from mining_system import *
+from mining_system import MiningSystem
 
 class MeshTerrain:
-    def __init__(this):
+    def __init__(this, pos, cam):
         this.subsets = []
         this.numSubsets = 128
         # must be even see gen terrain
@@ -20,10 +20,14 @@ class MeshTerrain:
         # our vertex dictionary  --- for mining
         this.vertexDic = {}
         this.perlin = Perlin()
+        this.mining = MiningSystem()
         for i in range(0, this.numSubsets):
             e = Entity(model = Mesh(), texture = this.textureAtlas)
             e.texture_scale*=64/e.texture.width
             this.subsets.append(e)
+    def update(this, pos, cam):
+        
+        this.mining.highlight(pos, cam, this.terrainDic)
     def getDic(this, dic, _x, _y, _z):
         return dic.get('x' + str(floor(_x)) + 'y' + str(floor(_y)) + 'z' + str(floor(_z)))
     def recDic(this, dic, _x, _y, _z, _rec):
@@ -65,8 +69,40 @@ class MeshTerrain:
         elif _y < -2:
             uu = 9 
             uv = 7
+            this.genWaterBlock(_x, _y + 1, _z)
         else:
             uu = 8
             uv = 7
         model.uvs.extend([Vec2(uu, uv) + u for u in this.block.uvs])
+        
+    def genWaterBlock(this, _x, _y, _z, subset=-1):
+        if _y < -1:
+            if subset == -1:
+                subset = this.currentSubset
+            # Extend to the vertices of our model, or first subset
+            model = this.subsets[subset].model
+            model.vertices.extend([Vec3(_x,_y,_z) + v for v in this.block.vertices])
+            # record terrain in dictionary
+            this.recDic(this.terrainDic, _x, _y, _z, "w")
+            # record subet index and first vertext of the block. 
+            # vob = (subset, len(model.vertices) - 37)
+            # this.recDic(this.vertexDic, _x, _y, _z, vob)
+            # decide random tint for color of block
+            c = random() - 0.5
+            model.colors.extend((Vec4(1-c, 1-c, 1-c, 1),) * this.numVertices)
+            # if _y > 2:
+            # # texture atlas at coord for grass
+            #     uu = 8
+            #     uv = 6
+            uu = 9 
+            uv = 7
+            if _y < -2:
+                _y += 1
+                this.genWaterBlock(_x, _y, _z)
+            # else:
+            #     uu = 8
+            #     uv = 7
+            model.uvs.extend([Vec2(uu, uv) + u for u in this.block.uvs])
+            
+        
         
