09e1a02b73f83f6e889ab33bdc5392b21f0a6dbd
Liam Cammpbell <liam22campbell@gmail.com>
2023-04-07 15:19:04 -0700

    Highting  of selected block appears!

---
+++ mining_system.py
@@ -0,0 +1,30 @@
+from ursina import Entity, color, floor
+from dictionary_craft import DictionaryCraft
+class MiningSystem:
+    def __init__(this):
+      # this lots of red and green make yellow, 0.4 makes it shiny
+      this.bte = Entity(model='cube', color=color.rgba(1,1,0,0.4))
+      this.bte.scale = 1.001
+      this.parseDict = DictionaryCraft()
+    def highlight(this, pos, camera, terrainDic):
+      print(pos)
+      for i in range(15, 1, -1):
+        wp = pos + camera.forward * i 
+        print(wp)
+        x = floor(wp.x)
+        y = floor(wp.y + 3)
+        z = floor(wp.z)
+        this.bte.y = y + 0.5
+        this.bte.z = z
+        this.bte.x = x
+        c = this.parseDict.getDictionary(terrainDic, x, y, z)
+        print(c)
+        print(this.bte.visible)
+        if this.parseDict.getDictionary(terrainDic, x, y, z) == 't': 
+          this.bte.visible  = True
+          
+          break
+        else:
+          this.bte.visible = False
+        
+      
