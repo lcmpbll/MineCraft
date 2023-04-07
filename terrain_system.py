d0a2338c6114dd09295341f5533b8e8c28ea290e
Liam Cammpbell <liam22campbell@gmail.com>
2023-04-06 23:32:58 -0700

    Add swirlEngine to build larger terrains and water

--- terrain_system.py
+++ terrain_system.py
@@ -2,16 +2,19 @@
 from ursina import *
 from random import randrange, random
 from perlin import Perlin
+from swirl_engine import SwirlEngine
 
 class MeshTerrain:
     def __init__(this):
         this.subsets = []
-        this.numSubsets = 1
-        this.subWidth = 128
+        this.numSubsets = 128
+        # must be even see gen terrain
+        this.subWidth = 4
+        this.currentSubset = 0
+        this.swirlEngine = SwirlEngine(this.subWidth)
         this.block = load_model('block.obj')
         this.textureAtlas = 'texture_atlas_3.png'
         this.numVertices = len(this.block.vertices)
-        print(this.numVertices)
         this.terrainDic = {}
         this.perlin = Perlin()
         for i in range(0, this.numSubsets):
@@ -23,17 +26,24 @@ class MeshTerrain:
     def recTerrainDic(this, _x, _y, _z, _rec):
         this.terrainDic['x' + str(floor(_x)) + 'y' + str(floor(_y)) + 'z' + str(floor(_z))] = _rec
     def genTerrain(this):
-        x = 0
-        z = 0
+        # get current position as we swirl around the world
+        x = floor(this.swirlEngine.pos.x)
+        z = floor(this.swirlEngine.pos.y)
         d = int(this.subWidth * 0.5)
         for k in range(-d, d):
             for j in range(-d, d):
                 y = floor(this.perlin.getHeight(x+k, z+j))
-                this.genBlock(x+k, y, z+j)
-        this.subsets[0].model.generate()      
+                if this.getTerrainDic(x+k, y, z+j) == None:
+                    this.genBlock(x+k, y, z+j)
+        this.subsets[this.currentSubset].model.generate() 
+        if this.currentSubset < this.numSubsets -1:
+            this.currentSubset += 1 
+        else:
+            this.currentSubset = 0 
+        this.swirlEngine.move()   
     def genBlock(this, _x, _y, _z):
         # Extend to the vertices of our model, or first subset
-        model = this.subsets[0].model
+        model = this.subsets[this.currentSubset].model
         model.vertices.extend([Vec3(_x,_y,_z) + v for v in this.block.vertices])
         # record terrain in dictionary
         this.recTerrainDic(_x, _y, _z, "t")
@@ -44,9 +54,9 @@ class MeshTerrain:
         # texture atlas at coord for grass
             uu = 8
             uv = 6
-        # elif _y < -2:
-        #     uu = 8 
-        #     uv = 8
+        elif _y < -2:
+            uu = 9 
+            uv = 7
         else:
             uu = 8
             uv = 7
