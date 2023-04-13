56e7fd026487aa952553872d5b1e6ffc7e65d3f4
Liam Cammpbell <liam22campbell@gmail.com>
2023-04-12 22:01:34 -0700

    BugFix, remove hanging str

--- dictionary_craft.py
+++ dictionary_craft.py
@@ -3,6 +3,6 @@ class DictionaryCraft:
     def __init__(this):
       pass
     def getDictionary(this, dic, _x, _y, _z):
-        return dic.get((floor(_x), floor(_y), 'z' + floor(_z)))
+        return dic.get((floor(_x), floor(_y), floor(_z)))
     def recDictionary(this, dic, _x, _y, _z, _rec):
         dic[(floor(_x), floor(_y), floor(_z))] = _rec
