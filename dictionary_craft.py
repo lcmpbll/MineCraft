d049e19da1e5444a8b4c36cf479687d8722ab497
Liam Cammpbell <liam22campbell@gmail.com>
2023-04-12 21:49:29 -0700

    Improve dictionary look up remove commented out code.

--- dictionary_craft.py
+++ dictionary_craft.py
@@ -3,8 +3,6 @@ class DictionaryCraft:
     def __init__(this):
       pass
     def getDictionary(this, dic, _x, _y, _z):
-        return dic.get('x' + str(floor(_x)) + 'y' + str(floor(_y)) + 'z' + str(floor(_z)))
+        return dic.get((floor(_x), floor(_y), 'z' + floor(_z)))
     def recDictionary(this, dic, _x, _y, _z, _rec):
-        dic['x' + str(floor(_x)) + 'y' + str(floor(_y)) + 'z' + str(floor(_z))] = _rec
-    def parseForDic(this, _x, _y, _z):
-        return 'x' + str(floor(_x)) + 'y' + str(floor(_y)) + 'z' + str(floor(_z))
+        dic[(floor(_x), floor(_y), floor(_z))] = _rec
