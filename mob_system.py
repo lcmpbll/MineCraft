ef8ca623ed18af370c43ba73b8341d12a71f9468
Liam Cammpbell <liam22campbell@gmail.com>
2023-04-15 16:57:36 -0700

    Smooth mob look at

--- mob_system.py
+++ mob_system.py
@@ -3,11 +3,11 @@ from ursina import *
 
 
 grey = FrameAnimation3d('panda_walk_', fps=1)
-#guy = Entity(model='panda_mod', texture='panda_tex')
-grey.texture = 'panda_tex.png'
+#WIP texture
+grey.texture='panda_texture'
 # grey.position = copy(subject.position) + Vec3(-6, -1, 9)
 grey.position = Vec3(-6, -1, 9)
-grey.turnSpeed = 0.2
+grey.turnSpeed = 2
 grey.speed = 1
 grey.step = 4
 grey.canSwim = False
@@ -17,7 +17,7 @@ grey.chaseDistance = 8
 def mob_movement(mob, _subjectPos, _terrainDic):
   #WIP chasing
   dist = _subjectPos - mob.position
-  print(mob.isChasing)
+
   if dist.length() > mob.chaseDistance:
     mob.isChasing = False
     mob.is_playing = False
@@ -26,8 +26,10 @@ def mob_movement(mob, _subjectPos, _terrainDic):
     mob.isChasing = True
   #turn speed not affected? BUG
   if mob.isChasing == True:
-    mob.lookAt(_subjectPos, mob.turnSpeed * time.dt)
+    tempOR = mob.rotation_y
+    mob.lookAt(_subjectPos)
     mob.rotation = Vec3(0, mob.rotation_y + 180, 0)
+    mob.rotation_y = lerp(tempOR, mob.rotation_y, mob.turnSpeed * time.dt)
     # now move towards target
     mob.position -= mob.forward * mob.speed * time.dt
     mob.resume()
@@ -36,29 +38,34 @@ def mob_movement(mob, _subjectPos, _terrainDic):
     
 
 def terrain_walk(mob, _terrainDic):
-    blockFound = False
-    
-    height = 1
-    # Technically flooring twice, but prevents shaking
-    x = floor(mob.x + 0.5)
-    y = floor(mob.y + 0.5)
-    z = floor(mob.z + 0.5)
-    for i in range(-mob.step, mob.step):
-        if _terrainDic.get(( x, y + i, z)) == 't': 
-            if _terrainDic.get(( x, y+i + 1, z)) == 't':
-                target = y + i + 1 + height
-                blockFound = True
-                break    
-            target = y + i + height
-            blockFound = True
-            break
-        elif _terrainDic.get((x, y, z)) == 'w':
-            if mob.canSwim == False:
-                mob.rotation = Vec3(0, mob.rotation_y + 180, 0)
-                mob.position -= mob.forward * mob.speed * time.dt
-    if blockFound == True: 
-        # step up or down : >
-        mob.y = lerp(mob.y, target, 6 * time.dt)
-    else: 
-        #gravity fall : <
-        mob.y -= 9.8 * time.dt 
+  blockFound = False
+  # add in variable to swich blocks and go somewhere else if water.
+  height = 1
+  if mob.y < -100:
+      mob.y = 100
+      print("Help I've fallen")
+  # Technically flooring twice, but prevents shaking
+  x = floor(mob.x + 0.5)
+  y = floor(mob.y + 0.5)
+  z = floor(mob.z + 0.5)
+  for i in range(-mob.step, mob.step):
+      if _terrainDic.get(( x, y + i, z)) == 't': 
+          if _terrainDic.get(( x, y+i + 1, z)) == 't':
+              target = y + i + 1 + height
+              blockFound = True
+              break    
+          target = y + i + height
+          blockFound = True
+          break
+      elif _terrainDic.get((x, y, z)) == 'w':
+          if mob.canSwim == False:
+              mob.rotation = Vec3(0, mob.rotation_y - 180, 0)
+              mob.position -= mob.forward * mob.speed * time.dt
+              
+              blockFound = True
+  if blockFound == True: 
+      # step up or down : >
+      mob.y = lerp(mob.y, target, 6 * time.dt)
+  else: 
+      #gravity fall : <
+      mob.y -= 9.8 * time.dt 
