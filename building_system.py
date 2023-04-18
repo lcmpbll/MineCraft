e2f07aeeb8d67d80ad77ed1fbb4ca10e332dc402
Liam Cammpbell <liam22campbell@gmail.com>
2023-04-18 14:36:44 -0700

    BugFix, cam and subpos switched in params

--- building_system.py
+++ building_system.py
@@ -2,36 +2,35 @@ from ursina import Vec3, floor
 """
 Our building system
 """
-def checkBuild(_bsite, _terrainDic, _camF, _sub_head):
-    # camF is camera forward
-    #subPos is players position
-    #adjust bsite for build tool entity offset. No longer have to do this because using the same model
-    # create some sort of vector from the players eyes to the highlighted block.
-    #l for line
-    dist = _bsite - _sub_head
-    j = 0.75
-    mouseInWorld = _sub_head + _camF * dist.length() 
-    mouseInWorld -= _camF * j
-    # _bsite += Vec3(0, -0.5, 0)
-    # we want to decide where to build a new block based on where we're looking
-    # x = floor(_bsite.x)
-    # y = floor(_bsite.y +1)
-    # z = floor(_bsite.z)
-    x = round(mouseInWorld.x)
-    y = floor(mouseInWorld.y)
-    z = round(mouseInWorld.z)
-    # if we are trying to build inside bte? 
-    if _bsite == Vec3(x, y, z): 
-      y += 1
-    #first check there isn't already terrain there y + 1 because y is one below the site
-    if _terrainDic.get((x, y, z)) != 'a' and _terrainDic.get((x, y, z)) != 'g':
-      if _terrainDic.get((x, y, z)) != None:
-        print('Cannot build here sorry')
-        return None
-      
+def checkBuild(_bsite, _terrainDic, _camF, _pos):
+  # camF is camera forward
+  #subPos is players position
+  #adjust bsite for build tool entity offset. No longer have to do this because using the same model
+  # create some sort of vector from the players eyes to the highlighted block.
+  dist = _bsite - _pos + Vec3(0, 1.62, 0)
+  print(dist.length())
+  
+  mouseInWorld = _pos + _camF * dist.length()
+  mouseInWorld -= _camF * 0.75
+  x = round(mouseInWorld.x)
+  y = floor(mouseInWorld.y)
+  z = round(mouseInWorld.z)
+
+  print(x, y, z)
+  # Oh, but what if we're trying to build inside bte?
+  # Build 1 above current block!
+  if _bsite == Vec3(x,y,z):
+      y+=1
+
+  #first check there isn't already terrain there y + 1 because y is one below the site
+  if _terrainDic.get((x, y, z)) != 'a' and _terrainDic.get((x, y, z)) != 'g':
+    if _terrainDic.get((x, y, z)) != None:
+      print('Cannot build here sorry')
+      return None
     
-    # can build if we're here
-    return Vec3(x, y, z)
+  
+  # can build if we're here
+  return Vec3(x, y, z)
 
 def gapShell(_bsite, _terrainDic):
   #what position
