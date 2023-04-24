6c097d9d8ddd450d88ed4208b3a25391cdf95c78
Liam Cammpbell <liam22campbell@gmail.com>
2023-04-23 17:55:34 -0700

    Add more variation to terrain

--- perlin.py
+++ perlin.py
@@ -2,16 +2,30 @@ from perlin_noise import PerlinNoise
 
 class Perlin:
     def __init__(this):
+        # og values ord('y') ord('o')
         this.seed = ord('y') + ord('o')
         #differneces
+        # 8
         this.octaves = 8
         # how often things change /\ vs _/-\_
+        # 256
         this.freq = 256
+        this.freq_continental = 512
         # how high or low
-        this.amp = 24
-        this.pNoise = PerlinNoise(seed=this.seed, octaves=this.octaves)
+        # 24
+        this.amp = 18
+        this.amp_continental = 128
+        this.pNoise_details = PerlinNoise(seed=this.seed, octaves=this.octaves)
+        this.pNoise_continental = PerlinNoise(seed=this.seed, octaves=1)
     def getHeight(this, _x, _z):
+        from math import sin
         y = 0 
-        y = this.pNoise([_x/this.freq, _z/this.freq]) * this.amp
+        # add greater variation
+        y = this.pNoise_continental([_x/this.freq_continental, _z/this.freq_continental]) * this.amp_continental
+        y += this.pNoise_details([_x/this.freq, _z/this.freq]) * this.amp
+        # follow a wave with surface variation
+        sAmp = 0.33
+        y += sin(_z) * sAmp
+        y += sin(_x * 0.5) * sAmp
         return y
         
