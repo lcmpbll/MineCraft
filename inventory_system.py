812105d3a82bb37ad8dda1680f1130a22f81c730
Liam Cammpbell <liam22campbell@gmail.com>
2023-04-25 15:08:33 -0700

    BugFix, hotbar scales.

--- inventory_system.py
+++ inventory_system.py
@@ -5,10 +5,13 @@ from config import *
 hotBarModel=load_model('quad',use_deepcopy=True)
 hotbar = Entity(model=hotBarModel, parent=camera.ui)
 # set size and position
-hotbar.scale_y = 0.08
-hotbar.scale_x = 0.68
-hotbar.position.y = -0.45 + (hotbar.scale_y * 0.5)
+# hotbar.scale_y = 0.08
+# hotbar.scale_x = 0.68
 # set appearance
+hotbar.scale=Vec3(0.68,0.08,0)
+hotbar.position.y = -0.45 + (hotbar.scale_y * 0.5)
+# ui_cols=hotbar.scale[0]/9
+hotbar.y=(-0.45 + (hotbar.scale_y*0.5))
 hotbar.color = color.dark_gray
 
 class Hotspot(Entity):
@@ -21,7 +24,7 @@ class Hotspot(Entity):
     this.model='quad'
     this.parent=camera.ui
     this.scale_y=Hotspot.scalar
-    this.scae_x = this.scale_y
+    this.scale_x = this.scale_y
     this.color=color.white
     this.texture='white_box'
     this.onHotbar = False
@@ -36,7 +39,7 @@ class Item(Draggable):
     this.model=load_model('quad',use_deepcopy=True)
     this.color=color.white
     this.scale_x = Hotspot.scalar*0.9
-    this.scale_y =  this.scale_y
+    this.scale_y =  this.scale_x
     this.visible=False
     this.onHotBar=False
     this.texture ='texture_atlas_3'
