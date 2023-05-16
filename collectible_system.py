2fd3ea0d175e97df5f38686ec323ecc4bf80e806
Liam Cammpbell <liam22campbell@gmail.com>
2023-05-15 21:38:23 -0700

    Add system to check wether subject has stepped on a collectible.

--- collectible_system.py
+++ collectible_system.py
@@ -1,10 +1,10 @@
 """
 System for mined blocks dropping collectable materials.
 """
-from ursina import Entity, Vec2, Vec4, load_model, math, time
+from ursina import Entity, Vec2, Vec3, Vec4, load_model, math, time, destroy
 from config import minerals
 from random import random
-from math import sin
+from math import sin, floor
 
 
 
@@ -13,25 +13,29 @@ from math import sin
 
 # WIP change to class
 class Collectible(Entity):
-  collectablesDic = {}
+  #collectablesDic = {}
 
-  def __init__(this, _blockType, _pos, _tex ):
+  def __init__(this, _blockType, _pos, _tex, _subject ):
     super().__init__()
     this.model = load_model('block.obj', use_deepcopy=True)
+    this.original_position = _pos
     this.texture = _tex
     this.position = _pos
+    this.subject = _subject
     this.numVerticies = len(this.model.vertices)
     this.blockType = _blockType
     this.collectables = []
     this.texture_scale *= 64/this.texture.width
     this.scale = 0.33
     this.shade = 1
+    this.rotation_speed = 2
     this.timeStamp = time.time()
     # record before adjusting position
-    Collectible.collectablesDic[this.position] = this
+    #Collectible.collectablesDic[this.position] = this
     this.y += 0.5 - (this.scale_y * 0.5)
     # put in after adjusting position
     this.original_y = this.position.y
+    this.is_bouncing = True
     this.drop_collectible()
   def drop_collectible(this):
     uu = minerals[this.blockType][0]
@@ -51,20 +55,31 @@ class Collectible(Entity):
     this.model.generate()
   def update(this):
     this.bounce()
+    this.checkPickUp()
     this.degrade_collectables()
-    
+  
+  def checkPickUp(this):
+    # we need to keep checking where the subject is
+    x = round(this.subject.position.x)
+    y = floor(this.subject.position.y)
+    z = round(this.subject.position.z)
+    #if Collectible.collectablesDic.get((x, y, z)) != None:
+    if Vec3(x, y, z) == this.original_position:
+      destroy(this)
      
   def degrade_collectables(this):
-    this.shade -= 0.05
-    if this.shade == 0:
-      Collectible.collectablesDic[this] = None
-      this.destroy()
+    this.shade -= 0.0005
+    this.rotation_speed += 0.002
+    if this.shade <= 0:
+      #Collectible.collectablesDic[this.position].pop()
+      destroy(this)
+
       
   def bounce(this):
     # adds a small bounce
-    this.rotation_y += 2
-    this.y = (this.original_y + sin(this.rotation_y * 0.05)* this.scale_y)
-  
+    this.rotation_y += this.rotation_speed
+    if this.is_bouncing == True:
+      this.y = (this.original_y + sin(this.rotation_y * 0.05)* this.scale_y)
       
 
 
