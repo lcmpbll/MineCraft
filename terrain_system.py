36444807010d88ea87f0793b86787d370b7a330d
Liam Cammpbell <liam22campbell@gmail.com>
2023-05-20 22:10:30 -0700

    adjust to 2 spaces.

--- terrain_system.py
+++ terrain_system.py
@@ -12,102 +12,102 @@ from tree_system import *
 # check what happens to the block beneath when building
 class MeshTerrain:
     def __init__(this, subject, cam):
-        this.subsets = []
-        this.numSubsets = 1024
-        # passed in from main 
-        this.sub = subject
-        this.cam = cam
-        # must be even see gen terrain
-        this.subWidth = 6
-        this.currentSubset = 0
-        this.swirlEngine = SwirlEngine(this.subWidth)
-        this.block = load_model('block.obj', use_deepcopy=True)
-        this.textureAtlas = 'texture_atlas_3.png'
-        this.numVertices = len(this.block.vertices)
-        this.terrainDic = {}
-        # our vertex dictionary  --- for mining
-        this.vertexDic = {}
-        this.perlin = Perlin()
-        this.setup_subsets()
+      this.subsets = []
+      this.numSubsets = 1024
+      # passed in from main 
+      this.sub = subject
+      this.cam = cam
+      # must be even see gen terrain
+      this.subWidth = 6
+      this.currentSubset = 0
+      this.swirlEngine = SwirlEngine(this.subWidth)
+      this.block = load_model('block.obj', use_deepcopy=True)
+      this.textureAtlas = 'texture_atlas_3.png'
+      this.numVertices = len(this.block.vertices)
+      this.terrainDic = {}
+      # our vertex dictionary  --- for mining
+      this.vertexDic = {}
+      this.perlin = Perlin()
+      this.setup_subsets()
         
     
     def plantTree(this, _x, _y, _z):
         
-        ent = TreeSystem.genTree(_x, _y, _z)
-        treeH =  round(ent * 10)
-        if ent == 0: 
-            return 
-        else:
-            for i in range(treeH):
-               # Trunk
-               this.genBlock(_x, _y + i, _z, blockType='concrete')
-            for t in range(-2, 3):
-               for tt in range(4):
-                   for ttt in range(-2, 3):
-               #crown
-                        this.genBlock(_x +  t, _y + treeH + tt, _z + ttt, blockType='emerald')
+      ent = TreeSystem.genTree(_x, _y, _z)
+      treeH =  round(ent * 10)
+      if ent == 0: 
+        return 
+      else:
+        for i in range(treeH):
+          # Trunk
+          this.genBlock(_x, _y + i, _z, blockType='concrete')
+        for t in range(-2, 3):
+          for tt in range(4):
+            for ttt in range(-2, 3):
+            #crown
+              this.genBlock(_x +  t, _y + treeH + tt, _z + ttt, blockType='emerald')
         
     def setup_subsets(this):
-       # instanciate subset entities
-        for i in range(0, this.numSubsets):
-            e = Entity(model = Mesh(), texture = this.textureAtlas)
-            e.texture_scale*=64/e.texture.width
-            this.subsets.append(e)
+      # instanciate subset entities
+      for i in range(0, this.numSubsets):
+        e = Entity(model = Mesh(), texture = this.textureAtlas)
+        e.texture_scale*=64/e.texture.width
+        this.subsets.append(e)
     def do_mining(this):
-        #pass in texture atlas for dropping collectable, see mine system
-        epi = mine(this.terrainDic, this.vertexDic, this.subsets, this.textureAtlas, this.sub)
-        if epi != None:
-            this.genWalls(epi[0], epi[1])
-            this.subsets[epi[1]].model.generate()
+      #pass in texture atlas for dropping collectable, see mine system
+      epi = mine(this.terrainDic, this.vertexDic, this.subsets, this.textureAtlas, this.sub)
+      if epi != None:
+        this.genWalls(epi[0], epi[1])
+        this.subsets[epi[1]].model.generate()
     def input(this, key):
-        if key == 'left mouse up' and bte.visible == True and mouse.locked == True:
-           this.do_mining()
-        if key=='right mouse up' and bte.visible==True and mouse.locked == True:
-            # do not build if empty handed.
-            if this.sub.blockType is None: return
-            buildSite = checkBuild( bte.position,
-                                    this.terrainDic,
-                                    this.cam.forward,
-                                    this.sub.position+Vec3(0,this.sub.height,0))
-            if buildSite != None:
-                this.genBlock(floor(buildSite.x), floor(buildSite.y), floor(buildSite.z), subset=0, building=True, blockType=this.sub.blockType)
-                this.subsets[0].model.generate()
-                gapShell(buildSite, this.terrainDic)
+      if key == 'left mouse up' and bte.visible == True and mouse.locked == True:
+        this.do_mining()
+      if key=='right mouse up' and bte.visible==True and mouse.locked == True:
+        # do not build if empty handed.
+        if this.sub.blockType is None: return
+        buildSite = checkBuild( bte.position,
+                                this.terrainDic,
+                                this.cam.forward,
+                                this.sub.position+Vec3(0,this.sub.height,0))
+        if buildSite != None:
+          this.genBlock(floor(buildSite.x), floor(buildSite.y), floor(buildSite.z), subset=0, building=True, blockType=this.sub.blockType)
+          this.subsets[0].model.generate()
+          gapShell(buildSite, this.terrainDic)
     def update(this):
-        highlight(this.sub.position, this.sub.height, this.cam, this.terrainDic)
-        #Blister mining == True
-        if bte.visible and mouse.locked == True:
-            #this is a for loop iterating over two variables
-            if held_keys['shift'] and held_keys['left mouse']:
-                this.do_mining()
+      highlight(this.sub.position, this.sub.height, this.cam, this.terrainDic)
+      #Blister mining == True
+      if bte.visible and mouse.locked == True:
+        #this is a for loop iterating over two variables
+        if held_keys['shift'] and held_keys['left mouse']:
+          this.do_mining()
             
                     
                     
     def getDic(this, dic, _x, _y, _z):
-        return dic.get((floor(_x), floor(_y), floor(_z)))
+      return dic.get((floor(_x), floor(_y), floor(_z)))
         
     def recDic(this, dic, _x, _y, _z, _rec):
-        dic[(floor(_x), floor(_y), floor(_z))] = _rec
+      dic[(floor(_x), floor(_y), floor(_z))] = _rec
         
     def genTerrain(this):
-        # get current position as we swirl around the world
-        x = floor(this.swirlEngine.pos.x)
-        z = floor(this.swirlEngine.pos.y)
-        d = int(this.subWidth * 0.5)
-        for k in range(-d, d):
-            for j in range(-d, d):
-                y = floor(this.perlin.getHeight(x+k, z+j))
-                if this.getDic(this.terrainDic, x+k, y, z+j) == None:
-                    this.genBlock(x+k, y, z+j)
-                    this.plantTree(x+k, y+1, z+j)
-                    
-        this.subsets[this.currentSubset].model.generate() 
-        if this.currentSubset < this.numSubsets -1:
-            this.currentSubset += 1 
-        else:
-            this.currentSubset = 0 
-        this.swirlEngine.move() 
-      
+      # get current position as we swirl around the world
+      x = floor(this.swirlEngine.pos.x)
+      z = floor(this.swirlEngine.pos.y)
+      d = int(this.subWidth * 0.5)
+      for k in range(-d, d):
+        for j in range(-d, d):
+          y = floor(this.perlin.getHeight(x+k, z+j))
+          if this.getDic(this.terrainDic, x+k, y, z+j) == None:
+            this.genBlock(x+k, y, z+j)
+            this.plantTree(x+k, y+1, z+j)
+                  
+      this.subsets[this.currentSubset].model.generate() 
+      if this.currentSubset < this.numSubsets -1:
+        this.currentSubset += 1 
+      else:
+        this.currentSubset = 0 
+      this.swirlEngine.move() 
+    
    
     def genBlock(this, _x, _y, _z, subset=-1, mining=False, building=False, blockType='soil'):
     
