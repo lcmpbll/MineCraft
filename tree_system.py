1c12bf1f316e1342f236e2ae2e724189b8a99189
Liam Cammpbell <liam22campbell@gmail.com>
2023-05-23 18:47:43 -0700

    Adjust freq, amp, and octaves.

--- tree_system.py
+++ tree_system.py
@@ -14,14 +14,14 @@ class TreeSystem():
   
   @staticmethod # not calling lots of instances, just this one method
   def setup():
-      Toctaves = 8
-      treeSeed = 2022
-      TreeSystem.amp = 10
-      # how frequently the trees peter out. 
-      TreeSystem.treeFreq = 256
-      
-      TreeSystem.noisyEnt = PerlinNoise(octaves=Toctaves, seed=treeSeed )
-      # TreeSystem.genTree(_x, _y, _z)
+    Toctaves = 8
+    treeSeed = 2022
+    TreeSystem.amp = 10
+    # how frequently the trees peter out. 
+    TreeSystem.treeFreq = 256
+    
+    TreeSystem.noisyEnt = PerlinNoise(octaves=Toctaves, seed=treeSeed )
+    # TreeSystem.genTree(_x, _y, _z)
     #create perlin noise method
     
   def genTree(_x, _y, _z):
